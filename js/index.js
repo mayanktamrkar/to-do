55dc1ee75994dece2189a45b7d5d0793542bf5b4
mayanktamrkar <mayanktamrkar786@gmail.com>
2022-11-10 12:46:58 +0530

    input_edit functionlity

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 function addinputtext() {
     let input_value=document.getElementById("inputv").value
     console.log("dfdf",input_value)
-    if(input_value[0]==undefined || input_value[0]===" "){
+    if(input_value[0]==undefined || input_value[0]==" "){
         
         document.getElementById("error").innerHTML="pls ente value"
         
@@ -16,7 +16,8 @@ function addinputtext() {
         let inputbox = document.createElement("input")
         inputbox.value = input_value
         inputbox.setAttribute("disabled", true);
-        inputbox.id="inputbox"
+       
+        inputbox.setAttribute("class", "input-box");
 
         let button_del = document.createElement('button')
         button_del.className="btn"
@@ -32,26 +33,45 @@ function addinputtext() {
         document.getElementById("inputv").value = ""
         let ul=document.querySelector("ul")
         console.log(ul)
+     
         ul.addEventListener("click",function del(e){
-            if(e.target.innerHTML=="edit"){
-                // let input_v =prompt("enter value",e.target.parentNode.firstChild.innerHTML)
-                document.getElementById("inputbox").removeAttribute("disabled")
-                let a=e.target.parentNode.firstChild
-                
-                
-                // let input_v=document.getElementById("inputbox").value
-                if(input_v!="" && input_v!=undefined){
+            if( e.target.innerHTML=="done"){
+
+                if(e.target.parentNode.firstChild.value.length!=0 && e.target.parentNode.firstChild.value[0]!==" ")
+                {
+                    
+                    
                     document.getElementById("error").innerHTML=""
-                    a.innerHTML=input_v
-                    document.getElementById("inputv").value=""
+                    let a=e.target.parentNode.firstChild
+                    a.setAttribute("disabled",true)
+                    a.setAttribute("class","input-box")
+                    e.target.innerHTML="edit"
+                    console.log("v",e.target.parentNode.firstChild.value)
 
                 }else{
-                    document.getElementById("error").innerHTML="pls enter value"
+                    document.getElementById("error").innerHTML="pls ente value"
+                
+
                 }
                 
+            
+
+            }else if(e.target.innerHTML=="edit"){
+                
+              
+                let a=e.target.parentNode.firstChild
+                a.removeAttribute("disabled")
+                a.removeAttribute("class")
+                e.target.innerHTML="done"
+             
+                
+                
+            
                
 
             }
+           
+           
              
            if(e.target.innerHTML=="delete"){
              e.target.parentNode.remove()
@@ -61,6 +81,9 @@ function addinputtext() {
          
           
         })
+        function lockinput(){
+         
+        }
     }
    
 
